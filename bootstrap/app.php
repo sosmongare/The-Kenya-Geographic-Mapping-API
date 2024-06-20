9563542684d6ee0098a3b6b64c738abef9150e99
Sospeter Mongare <sosmongare@gmail.com>
2024-06-20 15:52:25 +0300

    add vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\LogRequest::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
